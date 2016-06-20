acc9a1dad1552603bf036bfd6d479a1236d8c6b7
Wang Zhijun <so@idcf.jp>
2016-06-20 17:33:01 +0900

    Fix riak recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,7 @@ bash "install_riak_from_packagecloud" do
   curl -s https://packagecloud.io/install/repositories/basho/riak/script.rpm.sh | sudo bash
   yum install riak-#{node['riak']['major_number']}.#{node['riak']['minor_number']}.#{node['riak']['incremental']}-#{node['riak']['build']}.el7.centos.x86_64 -y
   EOH
+  not_if "which riak"
 end
 
 template "/etc/riak/riak.conf" do
