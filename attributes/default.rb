671ba8ceb74dbf37b08f67c8dcdc9a4a78ed26a5
Wang Zhijun <so@idcf.jp>
2016-06-20 16:31:05 +0900

    Fix riak recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['riak']['major_number'] = "2"
 default['riak']['minor_number'] = "1"
-default['riak']['increment_number'] = "4"
+default['riak']['incremental'] = "4"
 default['riak']['build'] = "1"
